ebb8e29160450df82dc2bd366c7b68d4de5887de
Mark Hallman <mw-hallman@sbcglobal.net>
2025-02-02 14:38:31 -0600

    Warning cleanup

--- src/main/java/com/markndevon/cardgames/websocket/security/filters/JwtFilter.java
+++ src/main/java/com/markndevon/cardgames/websocket/security/filters/JwtFilter.java
@@ -1,17 +1,12 @@
 package com.markndevon.cardgames.websocket.security.filters;
 
-import com.markndevon.cardgames.controller.UserController;
 import com.markndevon.cardgames.logger.Logger;
-import com.markndevon.cardgames.service.authentication.CardsUserDetailsService;
 import com.markndevon.cardgames.service.authentication.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
