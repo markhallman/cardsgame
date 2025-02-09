58d6fe3ccd6aa767cf6c4282e1eb0269a94e2a3b
Mark Hallman <mw-hallman@sbcglobal.net>
2025-02-09 16:18:48 -0600

    Add serialization logic for RulesConfig that determines GameState

--- src/main/java/com/markndevon/cardgames/model/config/RulesConfig.java
+++ src/main/java/com/markndevon/cardgames/model/config/RulesConfig.java
@@ -1,7 +1,19 @@
 package com.markndevon.cardgames.model.config;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "gameType"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = HeartsRulesConfig.class, name = "HEARTS")
+        // Add other game rule configurations here
+})
 public abstract class RulesConfig {
-    protected int numPlayers; // Number of players in the game
+    protected int numPlayers;
     public RulesConfig(int numPlayers) {
         this.numPlayers = numPlayers;
     }
